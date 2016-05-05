5f3f1da5eb070fc3505dbb1efb416526bdf42da5
Christine Quan <christine.quan@stanford.edu>
2016-05-05 00:04:58 -0700

    added some funtions to map.js

--- cs448b_crime/map.js
+++ cs448b_crime/map.js
@@ -1,3 +1,5 @@
+
+
 var homeMarker, workMarker, homeCircle, workCircle;
 var json = "https://dl.dropboxusercontent.com/s/souktjrm67okgkj/scpd_incidents.json?dl=0";
 var globalData;
@@ -127,3 +129,115 @@ function initialize() {
     });
 
 } // end of initializing
+
+
+function showInput(id, display) {
+
+    var message_entered =  document.getElementById(id).value;
+    document.getElementById(display).innerHTML = message_entered;
+}
+
+function updateTextInput(val) {
+      document.getElementById('textInput').value=val; 
+    }
+
+
+
+/*$(function() {
+  $( "#home_slider" ).slider({
+    orientation: "horizontal",
+    range: "min",
+    max: 10,
+    min: 1,
+    value: 10,
+    slide: function( event, ui ) {
+     updateRadius(circle, ui.value);
+    }
+
+
+     $( "#work_slider" ).slider({
+    orientation: "horizontal",
+    range: "min",
+    max: 10,
+    min: 1,
+    value: 10,
+    slide: function( event, ui ) {
+     updateRadius(circle, ui.value);
+    }
+});*/
+
+
+function updateRadius(circle, rad){
+  circle.setRadius(rad);
+}
+
+
+// Mapping the variables to bin them
+var CRIME_BIN = {
+  "ARSON": "PROPERTY",
+  "ASSAULT": "PERSONAL",
+  "BRIBERY": "FINANCIAL",
+  "BURGLARY": "PROPERTY",
+  "DISORDERLY CONDUCT": "AlCOHOL/DRUG",
+  "DRIVING UNDER THE INFLUENCE": "ALCOHOL/DRUG",
+  "DRUG/NARCOTIC": "ALCOHOL/DRUG",
+  "DRUNKENNESS": "ALCOHOL/DRUG",
+  "EMBEZZLEMENT": "FINANCIAL",
+  "EXTORTION": "PERSONAL",
+  "FAMILY OFFENSES": "PERSONAL",
+  "FORGERY/COUNTERFEITING": "FINANCIAL",
+  "FRAUD": "FINANCIAL",
+  "GAMBLING": "FINANCIAL",
+  "KIDNAPPING": "PERSONAL",
+  "LARCENY/THEFT": "PROPERTY",
+  "LIQUOR LAWS": "AlCOHOL/DRUG",
+  "LOITERING": "OTHER",
+  "MISSING PERSON": "PERSONAL",
+  "NON-CRIMINAL": "OTHER",
+  "OTHER OFFENSES": "OTHER",
+  "PROSTITUTION": "OTHER",
+  "ROBBERY": "PROPERTY",
+  "RUNAWAY": "PERSONAL",
+  "SECONDARY CODES": "OTHER",
+  "SEX OFFENSES, FORCIBLE": "PERSONAL",
+  "SEX OFFENSES, NON FORCIBLE": "PERSONAL",
+  "STOLEN PROPERTY": "PROPERTY",
+  "SUICIDE": "OTHER",
+  "SUSPICIOUS OCC": "OTHER",
+  "TRESPASS": "PROPERTY",
+  "VANDALISM": "PROPERTY",
+  "VEHICLE THEFT": "PROPERTY",
+  "WARRANTS": "OTHER",
+  "WEAPON LAWS": "OTHER"
+}
+
+// Visualize the data
+
+var see_points = [];
+var marker_images = [];
+
+var crime_categories_on = {
+  "PERSONAL": true,
+  "PROPERTY": true,
+  "FINANCIAL": true,
+  "ALCOHOL/DRUG": true,
+  "OTHER": true
+};
+
+
+function isCrimeTypeOn(type) {
+  var crime_type = CRIME_BIN[type];
+  return crimes_checked[crime_type];
+}
+
+function updatePoints (globalData, projection) {
+  visible_crime_data = globalData.filter(function(entry) {
+   
+    //check that crime category has been checked
+    var crime_type = entry["Category"];
+    if (!isCrimeTypeOn(crime_type)) {
+      return false;
+    }
+
+    return true;
+  });
